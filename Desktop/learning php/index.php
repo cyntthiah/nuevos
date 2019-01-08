d0dfa22c41b4ef908e0267e22fa497c6269ba278
raya <raya@ACA8023A.ipt.aol.com>
2019-01-08 16:15:37 -0300

    pruebas

---
+++ Desktop/learning php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>my first php page</h1>
+
+
+
+</body>
+
+
+<script src="app.php"></script>
+</html>
